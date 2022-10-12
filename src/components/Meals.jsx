95cfe21753c181f1bd32ed325d1e88d0736fad9f
Daniel Garcia Vizcaino <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-12 18:52:03 +0000

    Set up AppContext and add it to the Meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,10 @@
+import {useContext} from 'react'
+import {AppContext} from '../context'
+
+
 const Meals = () => {
+  const context = useContext(AppContext)
+  console.log(context)
   return <h1>Meals component</h1>
 }
 export default Meals
