7ca2052958b5608aa31f91460f4a7ab49897f5ba
Daniel Garcia Vizcaino <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-13 19:51:08 +0000

    Import the icon for like buttons

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,5 @@
 import {useGlobalContext} from '../context'
-
+import {BsHandThumbsUp} from 'react-icons/bs'
 
 const Meals = () => {
   const {meals} = useGlobalContext()
@@ -12,7 +12,7 @@ const Meals = () => {
         <img src={image} className="img"/>
         <footer>
           <h5>{title}</h5>
-          <button className="like-btn">click me </button>
+          <button className="like-btn"><BsHandThumbsUp /></button>
         </footer>
       </article>
     })}
