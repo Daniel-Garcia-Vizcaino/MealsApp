b9fc137bf4a782e4df2e05a715e1842b12b349dd
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-14 13:55:23 +0000

    Create the initial form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,13 @@
+import {useState} from 'react'
+import {useGlobalContext} from '../context'
+
 const Search = () => {
-  return <h1>Search component</h1>
+  return <header className="search-container">
+    <form>
+      <input type="text" placeholder="type favorite meal" className="form-input"></input>
+      <button type="submit" className="btn">search</button>
+      <button type="button" className="btn btn-hipster">surprise me!</button>
+    </form>
+  </header>
 }
 export default Search
