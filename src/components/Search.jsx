0afec5f81f5680c65f69ab047405ccf4d8592171
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-14 14:06:26 +0000

    Add functionality to the input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,21 @@ import {useState} from 'react'
 import {useGlobalContext} from '../context'
 
 const Search = () => {
+  const [text, setText] = useState("")
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return <header className="search-container">
-    <form>
-      <input type="text" placeholder="type favorite meal" className="form-input"></input>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="type favorite meal" value={text} onChange={handleChange} className="form-input"></input>
       <button type="submit" className="btn">search</button>
       <button type="button" className="btn btn-hipster">surprise me!</button>
     </form>
   </header>
 }
+
 export default Search
